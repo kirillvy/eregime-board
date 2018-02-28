1ade9a27fbfa137b755c7a477f5eff1053884b63
Kirill Vysokolov <kir2250@gmail.com>
2018-02-28 16:47:34 +0300

    added comments to javascript and reformatted parts of css

--- current.js
+++ current.js
@@ -1,3 +1,4 @@
+//changes the last post format for forums
 function changeLastTopic() {
     $('.c_last:contains(", By")').each(function() {
         $(this).html($(this).html().replace(/, By/g, '<br>In: ' + $(this).find('.c_last-title:first-child').prop('outerHTML') + '<br>By:'));
@@ -5,6 +6,7 @@ function changeLastTopic() {
     $('.c_last-title:first-child').remove();
 }
 
+//changes the last post format for topics
 function changeLastPost() {
     $('.row1, .row2').each(function() {
         $(this).children('.c_cat-title').append($(this).find('.cat-topicpages'));
@@ -14,34 +16,41 @@ function changeLastPost() {
     });
 }
 
+//moves quote button from the footer to the header
 function moveQuoteButton() {
     $('[id*=post-]').each(function() {
         $(this).find('.right').append($(this).next().next().next().find('.right').children());
     });
 }
 
+//reorganizes private messages and puts messages inside message header table
 function reorganizePM() {
     $('#pm_settings li:first-child').append($('#pm_folders'));
     $('#pm_viewer tbody').append('<tr><td id="inserter" colspan="4"></tr></td>')
     $('#inserter').append($('#conversation'));
 }
 
+//fades in on open
 function pageFunc() {
     $("#main, #foot_wrap, #copyright").fadeIn("fast");
 }
 
+//fades out on leave
 function closePage() {
     $("#main, #foot_wrap, #copyright").animate({
         opacity: 0
     }, "fast");
 }
 
+//fades back in on return
 function reopenPage() {
     $("#main, #foot_wrap, #copyright").animate({
         opacity: 1
     }, 400);
 }
 
+
+//checks page url and runs functions above
 if ($(location).is('[href*=/forum/], [href*=/index/]')) {
     changeLastTopic();
 }
@@ -55,30 +64,26 @@ if ($(location).is('[href*=/msg/]')) { //check if the URL is an inbox url
     reorganizePM(); // re-organizes the pm page
 }
 
-
+//fades in on page load
 $(function() {
     pageFunc();
 });
 
+//reopens page on return
 $(window).bind("pageshow", function() {
     if ($('#main').css('opacity') == 0) {
         reopenPage();
     }
 });
 
-$(window).bind("onbeforeunload", function() {
-    closePage();
-});
-
-
+//fades page on click
 $("a, :submit").click(function() {
     if ($(this).is('[href*=http://eregime.org], :not([href*=#], [id*=preview], [target=_blank])') && newWin == false) {
         closePage();
     }
 });
 
-var newWin = false;
-
+//opens a new window with emoticons instead of default dropdown window, prevents default behavior
 $("#emot_end").click(function() {
     var htmlHead = '<!DOCTYPE html> <meta charset=\"utf-8\"/> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <title>Emoticons ;o</title> <script type=\"text/javascript\" src=\"http://b3.ifrm.com/static/jq171.js?z=4\"><\/script> <style>tr{display: inline-grid; color: #ffffff;}img{cursor: pointer;}div{background-color: tomato; color: #ffffff; position: fixed; right: 0px; top: 0px; font-family: \"Segoe UI\", Tahoma, Geneva, Verdana, sans-serif; font-size: 1.5em; opacity: 0.8; margin: 0; cursor: pointer; line-height: 1.5em; padding: 0 6px; }div:hover{opacity:0.95;}</style> <div class=\"close\" onclick=\"window.close()\">&#10006;</div>';
     var htmlFoot = '<script>function insert_emot(text){var textarea=opener.document.getElementById(\"c_post-text\"); text=\" \" + text + \" \"; textarea.value=textarea.value + text;}$(\"img\").click(function(){insert_emot($(this).attr(\"alt\"));}) <\/script>';
@@ -90,6 +95,12 @@ $("#emot_end").click(function() {
     });
 })
 
+function allEmots() {
+    return;
+}
+
+//prevents fade if user opens a new window or tab (win/mac)
+var newWin = false;
 $(document).keydown(function(event) {
     if (event.ctrlKey || event.altKey || event.shiftKey || event.metaKey) {
         newWin = true;
@@ -111,6 +122,8 @@ $(document).mousedown(function(event) {
     }
 });
 
+//april fools easter egg
+
 var a1 = new Date();
 if (a1.getDate() == 1 && a1.getMonth() == 3) {
     $('body').append('<img class="logo" src="http://b2.ifrm.com/10755/76/0/a322065/avatar-322065.gif" alt="eregime"><div id="a1"><div>COPY DEEZ NUTZ</div></div>');
@@ -128,10 +141,4 @@ if (a1.getDate() == 1 && a1.getMonth() == 3) {
             top: e.pageY + 10
         });
     });
-}
-
-
-
-function allEmots() {
-    return;
 }
