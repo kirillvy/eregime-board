88811a5309ee30d5f327ca218561650b5342dea1
Kirill Vysokolov <kir2250@gmail.com>
2018-03-04 13:57:55 +0300

    minor js changes

--- current.js
+++ current.js
@@ -4,8 +4,8 @@
 
 //hides forums on the front page 
 function hideForums() {
-    if (typeof hideForumID !== 'undefined') {
-        hideForumID.forEach( function(element){
+    if (typeof hideForumID === 'object' && Array.isArray(hideForumID)) {
+        hideForumID.forEach(function (element) {
             $('#forum-' + element).next().next('tr:has(td.c_subforum)').hide();
             $('#forum-' + element + ' td.c_mark').attr('rowspan', '2');
         });
@@ -14,7 +14,7 @@ function hideForums() {
 
 //changes the last post format for forums
 function changeLastTopic() {
-    $('.c_last:contains(", By")').each(function() {
+    $('.c_last:contains(", By")').each(function () {
         $(this).html($(this).html().replace(/, By/g, '<br>In: ' + $(this).find('.c_last-title:first-child').prop('outerHTML') + '<br>By:'));
     });
     $('.c_last-title:first-child').remove();
@@ -22,7 +22,7 @@ function changeLastTopic() {
 
 //changes the last post format for topics
 function changeLastPost() {
-    $('.row1:has(.c_cat-lastpost), .row2:has(.c_cat-lastpost)').each(function() {
+    $('.row1:has(.c_cat-lastpost), .row2:has(.c_cat-lastpost)').each(function () {
         $(this).find('.description').before($(this).find('.cat-topicpages'));
         $(this).children('.c_cat-lastpost').html($(this).children('.c_cat-lastpost').html().replace(/Last Post By/g,
             '<strong><a href="' + $(this).find('.c_cat-title a:first-child').attr('href') + '" title="Last Post">Last Post By</a></strong>'));
@@ -32,7 +32,7 @@ function changeLastPost() {
 
 //moves quote button from the footer to the header
 function moveQuoteButton() {
-    $('[id*=post-]').each(function() {
+    $('[id*=post-]').each(function () {
         $(this).find('.right').append($(this).next().next().next().find('.right').children());
     });
 }
@@ -45,14 +45,16 @@ function reorganizePM() {
 }
 
 //opens a new window with emoticons instead of default dropdown window, prevents default behavior
-$("#emot_end").click(function() {
+$("#emot_end").click(function () {
     var htmlHead = '<!DOCTYPE html> <meta charset=\"utf-8\"/> <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"> <title>Emoticons ;o</title> <script type=\"text/javascript\" src=\"http://b3.ifrm.com/static/jq171.js?z=4\"><\/script> <style>tr{display: inline-grid; color: #ffffff;}img{cursor: pointer;}div{background-color: tomato; color: #ffffff; position: fixed; right: 0px; top: 0px; font-family: \"Segoe UI\", Tahoma, Geneva, Verdana, sans-serif; font-size: 1.5em; opacity: 0.8; margin: 0; cursor: pointer; line-height: 1.5em; padding: 0 6px; }div:hover{opacity:0.95;}</style> <div class=\"close\" onclick=\"window.close()\">&#10006;</div>';
     var htmlFoot = '<script>function insert_emot(text){var textarea=opener.document.getElementById(\"c_post-text\"); text=\" \" + text + \" \"; textarea.value=textarea.value + text;}$(\"img\").click(function(){insert_emot($(this).attr(\"alt\"));}) <\/script>';
-    $.get("../keys", function($txt) {
+    $.get("../keys", function ($txt) {
         $(this).attr('target', '_blank');
         var emotWindow = window.open('', 'emotWindow', "menubar=no,location=no,resizable=yes,scrollbars=yes,status=no,width=1024,height=768");
         emotWindow.document.write(htmlHead + $txt + htmlFoot);
-        $("img", emotWindow.document).click(function() { emot($(this).attr("title")); });
+        $("img", emotWindow.document).click(function () {
+            emot($(this).attr("title"));
+        });
     });
 })
 
@@ -66,8 +68,7 @@ function allEmots() {
 
 //checks page url and runs functions above
 if ($(location).is('[href*=/index/]')) {
-    if (typeof hideForumID !== 'undefined') {
-    hideForums();}
+    hideForums();
 }
 if ($(location).is('[href*=/forum/], [href*=/index/]')) {
     changeLastTopic();
@@ -102,12 +103,12 @@ function reopenPage() {
 }
 
 //fades in on page load
-$(function() {
+$(function () {
     openPage();
 });
 
 //reopens page on return
-$(window).bind("pageshow", function() {
+$(window).bind("pageshow", function () {
     if ($('#main').css('opacity') == 0) {
         reopenPage();
     }
@@ -119,10 +120,10 @@ $(window).bind("pageshow", function() {
 
 //prevents fade if user opens a new window or tab (win/mac)
 var newWin = false;
-$(document).keydown(function(event) {
+$(document).keydown(function (event) {
     if (event.ctrlKey || event.altKey || event.shiftKey || event.metaKey) {
         newWin = true;
-        $(document).keyup(function(event) {
+        $(document).keyup(function (event) {
             if (!event.ctrlKey && !event.altKey && !event.shiftKey && !event.metaKey) {
                 newWin = false;
             }
@@ -131,17 +132,17 @@ $(document).keydown(function(event) {
 
 });
 
-$(document).mousedown(function(event) {
+$(document).mousedown(function (event) {
     if (event.which == "3") {
         newWin = true;
-        $(document).mousedown(function(event) {
+        $(document).mousedown(function (event) {
             newWin = false;
         })
     }
 });
 
 //fades page on click
-$("a, :submit").click(function() {
+$("a, :submit").click(function () {
     if ($(this).is('[href*=http://eregime.org], [href*=/forumforticket/], :not([href*=#], [id*=preview], [target=_blank])') && newWin == false) {
         closePage();
     }
@@ -156,15 +157,15 @@ $("a, :submit").click(function() {
 var a1 = new Date();
 if (a1.getDate() == 1 && a1.getMonth() == 3) {
     $('body').append('<img class="mouselogo" src="http://b2.ifrm.com/10755/76/0/a322065/avatar-322065.gif" alt="eregime"><div id="a1"><div>COPY DEEZ NUTZ</div></div>');
-    $(document).keydown(function(event) {
+    $(document).keydown(function (event) {
         if (event.which == "67" && event.ctrlKey) {
             $("#a1").fadeIn("fast");
-            $(document).keyup(function(event) {
+            $(document).keyup(function (event) {
                 $("#a1").fadeOut("fast");
             });
         }
     });
-    $(document).mousemove(function(e) {
+    $(document).mousemove(function (e) {
         $('.mouselogo').offset({
             left: e.pageX + 10,
             top: e.pageY + 10
